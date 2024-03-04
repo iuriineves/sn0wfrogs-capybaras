9511f1591fc0f3152d8efdab5aa47ced5517ab36
yuri <iuriayolphineves@gmail.com>
2024-03-04 19:35:32 +0000

    thermal springs work! it was easier than i thought

--- src/main/java/iuriineves/neves_capybaras/block_entity/ThermalSpringBlockEntity.java
+++ src/main/java/iuriineves/neves_capybaras/block_entity/ThermalSpringBlockEntity.java
@@ -1,11 +1,9 @@
 package iuriineves.neves_capybaras.block_entity;
 
-import iuriineves.neves_capybaras.NevesCapybaras;
 import iuriineves.neves_capybaras.init.ModBlockEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
